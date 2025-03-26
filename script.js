46fe5e216a6d5696dba057f80e210e228713e0b5
Rishabh2 <rishabh.bhasin.33@gmail.com>
2025-03-25 23:13:14 -0700

    Add pickups

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ function roll() {
     const itemCount = 2;
     const floorCount = 5;
     const charCount = 2;
+    const pickupCount = 3;
 
     const floor = document.getElementById("floor");
     const character = document.getElementById("character");
     const item = document.getElementById("item");
 
+    const pickups = [document.getElementById("pickup1"), document.getElementById("pickup2"), document.getElementById("pickup3")];
+
     // topmin, topmax, leftmin, leftmax
     const itemranges = [100, 250, 50, 250];
     const charranges = [400, 500, 900, 1050];
+    const pickupranges = [100, 600, 50, 1100];
 
     floor.src = "floor" + rrange(1, floorCount).toString() + ".png";
 
@@ -26,6 +30,17 @@ function roll() {
     item.src = "item" + rrange(1, itemCount).toString() + ".png";
     item.style.top = rrange(itemranges[0], itemranges[1]).toString() + "px";
     item.style.left = rrange(itemranges[2], itemranges[3]).toString() + "px";
+
+    for (let pickup of pickups) {
+        pickup.src = "pickup" + rrange(1, pickupCount).toString() + ".png";
+        pickup.style.top = rrange(pickupranges[0], pickupranges[1]).toString() + "px";
+        pickup.style.left = rrange(pickupranges[2], pickupranges[3]).toString() + "px";
+        if (Math.random() < 0.5) {
+            pickup.style.display = "inline";
+        } else {
+            pickup.style.display = "none";
+        }
+    }
 
     console.log("roll end");
 }
