0e5008e4aaf54152df1e00acea2757298c3ee989
Rishabh2 <rishabh.bhasin.33@gmail.com>
2025-03-25 18:27:12 -0700

    Randomize item and character position

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function rrange(min, max) { // inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function roll() {
     console.log("roll start");
 
@@ -9,9 +13,19 @@ function roll() {
     const character = document.getElementById("character");
     const item = document.getElementById("item");
 
-    floor.src = "floor" + (Math.floor(Math.random() * floorCount) + 1).toString() + ".png";
-    character.src = "character" + (Math.floor(Math.random() * charCount) + 1).toString() + ".png";
-    item.src = "item" + (Math.floor(Math.random() * itemCount) + 1).toString() + ".png";
+    // topmin, topmax, leftmin, leftmax
+    const itemranges = [100, 250, 50, 250];
+    const charranges = [400, 500, 900, 1050];
+
+    floor.src = "floor" + rrange(1, floorCount).toString() + ".png";
+
+    character.src = "character" + rrange(1, charCount).toString() + ".png";
+    character.style.top = rrange(charranges[0], charranges[1]).toString() + "px";
+    character.style.left = rrange(charranges[2], charranges[3]).toString() + "px";
+
+    item.src = "item" + rrange(1, itemCount).toString() + ".png";
+    item.style.top = rrange(itemranges[0], itemranges[1]).toString() + "px";
+    item.style.left = rrange(itemranges[2], itemranges[3]).toString() + "px";
 
     console.log("roll end");
 }
